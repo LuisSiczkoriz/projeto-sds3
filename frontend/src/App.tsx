9c0a4ffd50458e04b822e09c111ea78334fdb0a7
Luis Siczkoriz <luis.siczkoriz@gmail.com>
2021-05-07 13:46:04 -0300

    Home, navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
-import Routes from "routes";
+//import BarChart from "components/BarChart";
+//import DataTable from "components/DataTable";
+//import DonutChart from "components/DonutChart";
+//import Footer from "components/Footer";
+//import NavBar from "components/NavBar";
+import Routes from "Routes";
+
 
 function App() {
   return (
